ff1272d3389f526bfa380b4d2ac71778d46042f4
陳建智 <rocky@chenjianzhideMac-mini.local>
2024-10-27 12:01:15 +0800

    jwt cookies_fix

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -31,7 +31,7 @@ export default async function handleRequest(
       'https://shopify.com',
       'https://www.google-analytics.com',
       'https://www.googletagmanager.com',
-      //'https://canvastalk-867062847423.asia-east1.run.app',
+      'https://canvastalk-867062847423.asia-east1.run.app',
       'https://40ed06-12.myshopify.com',
       'https://01ja74vd3j52xmzffynj6d1vdz-827ba860dd475bd1fc22.myshopify.dev',
       'https://monorail-edge.shopifysvc.com',
